14c43c1a95b60201e20dc8f4b170c90a3aefdec3
ICE K. <info.icejourney@gmail.com>
2024-12-05 19:43:04 +0700

    Add Alert and Button components with icons

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,56 @@
 // Start coding here
 
-function Alert() {}
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+// import ไอคอนจากไฟล์ภาพ
+import errorIcon from './icons/error-icon.png';
+import warningIcon from './icons/warning-icon.png';
+import infoIcon from './icons/info-icon.png';
+import successIcon from './icons/success-icon.png';
+
+function Alert(props) {
+  const alertStyles = css`
+    display: flex;
+    align-items: center;
+    padding: 16px;
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: bold;
+    margin: 8px 0;
+    background-color: ${props.type === 'error'
+      ? '#fdecea'
+      : props.type === 'warning'
+      ? '#fff4e5'
+      : props.type === 'info'
+      ? '#f0f4ff'
+      : '#e7f6e7'};
+    color: ${props.type === 'error'
+      ? '#d32f2f'
+      : props.type === 'warning'
+      ? '#ffa000'
+      : props.type === 'info'
+      ? '#1976d2'
+      : '#388e3c'};
+  `;
+
+  const icon = {
+    error: errorIcon,
+    warning: warningIcon,
+    info: infoIcon,
+    success: successIcon,
+  };
+
+  return (
+    <div css={alertStyles}>
+      <img
+        src={icon[props.type]}
+        alt={`${props.type} icon`}
+        style={{ width: '24px', height: '24px', marginRight: '12px' }}
+      />
+      {props.children}
+    </div>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,20 @@
 // Start coding here
 
-function Button() {}
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+function Button(props) {
+  const buttonStyles = css`
+    padding: 16px 32px;
+    font-size: 16px;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+    color: white;
+    background-color: ${props.variant === 'primary' ? '#064ee9' : '#08a4e8'};
+  `;
+
+  return <button css={buttonStyles}>{props.children}</button>;
+}
+
+export default Button;
